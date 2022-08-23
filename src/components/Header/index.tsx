829c3eb1aec4ad27ab8cafdf15e8d0bb361f44c4
Claylton Roque <claylton202@gmail.com>
2022-08-23 17:15:30 -0300

    é mole

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,12 @@
 import { HeaderContainer } from './styles'
 import { Timer, Scroll } from 'phosphor-react'
-import  Logoignite  from '../../assets/logo-ignite.svg'
+
 import { NavLink } from 'react-router-dom'
  
 export function Header() {
     return (
         <HeaderContainer>
-            <img src={Logoignite} alt="" />
+           
             <nav>
                 <NavLink to="/">
                     <Timer size={24} />
